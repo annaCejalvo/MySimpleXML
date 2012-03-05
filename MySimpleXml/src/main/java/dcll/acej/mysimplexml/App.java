0df6f9b0ba300a91d5762cbc6bb9f1492e90ef37
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-05 16:04:05 +0100

    #1 Issue #1

--- MySimpleXml/src/main/java/dcll/acej/mysimplexml/App.java
+++ MySimpleXml/src/main/java/dcll/acej/mysimplexml/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! bis" );
+        System.out.println( "Hello World!" );
         System.out.println( "Milestone Step1, Issue #1" );
     }
 }
